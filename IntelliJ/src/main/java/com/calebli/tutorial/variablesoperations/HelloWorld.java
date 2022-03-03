6b811dad9ce9a14ecd9629487b7696ac419489ad
ToothbrushB <49379586+ToothbrushB@users.noreply.github.com>
2022-03-03 12:10:20 -0600

    Variables and Operations

---
+++ IntelliJ/src/main/java/com/calebli/tutorial/variablesoperations/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.calebli.tutorial.variablesoperations;
+
+public class HelloWorld {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello World!");
+        Math.
+    }
+}
